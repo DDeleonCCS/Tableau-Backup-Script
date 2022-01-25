c44e117062e9e6249829cf15b91b82b2fea4827a
DDeleonCCS <98428647+DDeleonCCS@users.noreply.github.com>
2022-01-25 16:24:09 -0500

    script

---
+++ tableau-server-backup.bash
@@ -0,0 +1,99 @@
+#!/bin/bash
+######   Tableau Server on Linux Backup Script    ######
+###   Manual Run
+#			  sudo su -l <tsmusername> -c /var/opt/tableau/tableau_server/scripts/tableau-server-backup.bash 
+#
+###		Scheduled Run: 
+#       Run the script using cron to a regular basis
+#			  sudo su -l $tsmuser -c "crontab -e"
+#		
+#	  Example: to schedule it to run once a day at 01:00, add this to your crontab
+#		0 1 * * * /var/opt/tableau/tableau_server/scripts/tableau-server-backup.bash > /home/ddeleon/tableau-server-backup.log
+#   BY DEFAULT Amazon EC2 Linux Instance goes by UTC; Schedule chron based on UTC
+
+###########################################
+
+##### VARIABLES SECTION #####
+# Set some variables - you should change these to match your own environment
+
+# Set the date how we want it
+DATE=`date '+%d-%m-%Y'`
+# Grab the current datetime for timestamping the log entries
+TIMESTAMP=`date '+%Y-%m-%d %H:%M:%S'`
+# Copy backups to DR Cluster
+copy_backup="yes"
+# Set the location to where backup are placed 
+external_backup_path="/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
+# How many days do you want to keep old backup files for? 
+backup_days="7"
+# What do you want to name your backup files? (will automatically append current date to this filename)
+backup_name="tableau-server-backup"
+##### END OF VARIABLES SECTION #####
+
+###########################################
+
+##### LOAD ENVIRONMENT & USER INPUT #####
+# Load the Tableau Server environment variables into the cron environment
+source /etc/profile.d/tableau_server.sh
+
+# In case that doesn't work then this might do it 
+load_environment_file() {
+  if [[ -f /etc/opt/tableau/tableau_server/environment.bash ]]; then
+    source /etc/opt/tableau/tableau_server/environment.bash
+    env_file_exists=1
+  fi
+}
+
+if [ "$#" -eq 2 ] ; then
+	# Get tsm username from command line input
+	tsmuser="$1"
+	# Get tsm password from command line input
+	tsmpassword="$2" 
+	tsmparams="-u $tsmuser -p $tsmpassword"
+elif [ $(echo $TABLEAU_SERVER_DATA_DIR_VERSION | cut -d. -f1) -ge 20192 ]  && (id -nG | grep -q tsmadmin || [ ${EUID} -eq 0 ]) ; then 
+	# 2019.2 workflow. If running as tsmadmin member or root, do not set userinfo
+	declare tsmparams
+fi
+##### END OF LOAD ENVIRONMENT & USER INPUT #####
+
+###########################################
+
+##### BACKUP SECTION #####
+# get the path to the backups folder
+backup_path=$(tsm configuration get -k basefilepath.backuprestore $tsmparams)
+echo $TIMESTAMP "The path for storing backups is $backup_path" 
+
+# count the number of backup files eligible for deletion and output 
+echo $TIMESTAMP "Cleaning up old backups..."
+lines=$(find $backup_path -type f -regex '.*.\(tsbak\|json\)' -mtime +$backup_days | wc -l)
+if [ $lines -eq 0 ]; then 
+	echo $TIMESTAMP $lines old backups found, skipping...
+	else echo  $TIMESTAMP $lines old backups found, deleting...
+		#remove backup files older than N days
+		find $backup_path -type f -regex '.*.\(tsbak\|json\)' -mtime +$backup_days -exec rm -f {} \;
+fi
+
+#export current settings
+echo $TIMESTAMP "Exporting current settings..."
+tsm settings export -f $backup_path/settings-$DATE.json $tsmparams
+#create current backup
+echo $TIMESTAMP "Backup up Tableau Server data..."
+tsm maintenance backup -f $backup_name -d $tsmparams
+
+#obtaining the latest settings
+lastest_settings=$(ls -t $backup_path/*.json | head -n1)
+#obtaining the latest backup
+lastest_backup=$(ls -t $backup_path/*.tsbak | head -n1)
+
+#copy backups to different location (optional)
+if [ "$copy_backup" == "yes" ];
+	then
+	echo $TIMESTAMP "Copying backup and settings to remote share"
+	# You can change where to send the backups to by changing the user: username@10.80.3.217
+	scp $latest_settings ec2-user@10.80.3.217:$external_backup_path/
+  scp $latest_backup ec2-user@10.80.3.217:$external_backup_path/
+fi
+##### END OF BACKUP SECTION #####
+
+# END OF SCRIPT
+echo $TIMESTAMP "Backup completed"
